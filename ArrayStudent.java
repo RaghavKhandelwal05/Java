7d74471d18f7e31f59fde8c5173bbea6cad1b880
RaghavKhandelwal05 <110650979+RaghavKhandelwal05@users.noreply.github.com>
2022-08-05 22:28:14 +0530

    Add files via upload

---
+++ ArrayStudent.java
@@ -0,0 +1,26 @@
+//Q.9a) - Write a java program to read n Students names from user, store them into the 
+//        ArrayList collection. The program should not allow duplicate names. Display the 
+//        names in Ascending order.
+
+
+
+import java.util.*;
+class ArrayStudent
+{
+	public static void main(String args[])
+	{
+		int i,n;
+		String m;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter no. of Students");
+		n=sc.nextInt();
+		ArrayList<String>al=new ArrayList<String>();
+		for(i=0;i<n;i++)
+		{
+			m=sc.next();
+			al.add(m);
+		}
+		TreeSet<String>ts=new TreeSet<String>(al);
+		System.out.println(ts);
+	}
+}
